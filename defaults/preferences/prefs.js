31ebe4f2ddcdb25019d93fe7d2c63073b40a611c
Misha Chitharanjan <github@mishac.com>
2008-08-24 06:53:50 +0000

    preliminary support for transparent overlay preferences window (unfinished)

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,3 +2,4 @@ pref("extensions.findintabs.maxresults", 200);
 pref("extensions.findintabs.finddelay", 100);
 pref("extensions.findintabs.checkcompatability", true);
 pref("extensions.findintabs.showfavicon", true);
+pref("extensions.findintabs.showsearchoverlay", true);
