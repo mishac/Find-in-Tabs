8f9215fad26272519271db9df9992ec7f30db619
Michael Havas <michael.havas@gmail.com>
2008-12-06 02:41:22 +0000

    Fixed highlighting

---
+++ trunk/defaults/preferences/prefs.js
@@ -0,0 +1,5 @@
+pref("extensions.findintabs.maxresults", 200);
+pref("extensions.findintabs.finddelay", 100);
+pref("extensions.findintabs.checkcompatability", true);
+pref("extensions.findintabs.showfavicon", true);
+pref("extensions.findintabs.showsearchoverlay", false);
