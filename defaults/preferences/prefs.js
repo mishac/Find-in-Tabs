95fa1e7a911ed597bcf0077ca5838056462b3def
Michael Havas <michael.havas@gmail.com>
2008-12-11 03:13:47 +0000

    Removing overlay option

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,4 +2,3 @@ pref("extensions.findintabs.maxresults", 200);
 pref("extensions.findintabs.finddelay", 100);
 pref("extensions.findintabs.checkcompatability", true);
 pref("extensions.findintabs.showfavicon", true);
-pref("extensions.findintabs.showsearchoverlay", false);
